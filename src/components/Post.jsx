47a0ec604a1114d09897278b96679b873b81f7e9
StefanRCitiriga <73283856+StefanRCitiriga@users.noreply.github.com>
2024-02-04 18:14:49 +0200

    functionally complete

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,15 @@
-import classes from './Post.module.css'
+import classes from "./Post.module.css";
+import { Link } from "react-router-dom";
 
-const Post = ({author, body}) => {
-    return <li className={classes.post}>
+const Post = ({ id, author, body }) => {
+  return (
+    <li className={classes.post}>
+      <Link to={id}>
         <p className={classes.author}>{author}</p>
         <p className={classes.text}>{body}</p>
+      </Link>
     </li>
-}
+  );
+};
 
 export default Post;
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -6,21 +6,10 @@ const PostList = () => {
   
   const posts = useLoaderData();
 
-  function addPostHandler(postData){
-    fetch('http://localhost:8080/posts', {
-      method: 'POST',
-      body: JSON.stringify(postData),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    setPosts((prevState) => [postData, ...prevState]);
-  }
-
   return (
     <>
       {posts.length > 0 && <ul className={classes.posts}>
-        {posts.map((post) => <Post key={post.body} author={post.author} body={post.body}/>)}
+        {posts.map((post) => <Post key={post.id} id={post.id} author={post.author} body={post.body}/>)}
       </ul>}
       {posts.length === 0 && <div style={{textAlign: 'center', color:'white'}}>
         <h2>There are no posts yet.</h2>
